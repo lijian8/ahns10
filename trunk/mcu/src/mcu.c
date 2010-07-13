d7d0c79544a58df72779fb22979eb3d610be5c04
tlmolloy@gmail.com <tlmolloy@gmail.com@9b9702f6-20ff-11df-95c0-0d452a75c466>
2010-07-13 15:01:25 +0000

    mcu code base

---
+++ trunk/mcu/src/mcu.c
@@ -0,0 +1,29 @@
+/**
+ * \file   mcu.c
+ * \author Tim Molloy
+ *
+ * $Author: tlmolloy $
+ * $Date: 2010-06-10 23:59:05 +1000 (Thu, 10 Jun 2010) $
+ * $Rev: 164 $
+ * $Id: main.cpp 164 2010-06-10 13:59:05Z tlmolloy $
+ *
+ * Queensland University of Technology
+ *
+ * \section DESCRIPTION
+ * Main file for the ATMEGA328P Mode Control Unit
+ */
+
+#include <avr/io.h>
+
+#define TRUE 1
+#define FALSE 0
+
+void main (void)
+{
+  for ( ; ; )
+  {
+        
+  }
+
+  return;
+}
