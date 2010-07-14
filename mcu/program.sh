78982538136b7256bcae5bf4fc47f8366aad2f4c
tlmolloy@gmail.com <tlmolloy@gmail.com@9b9702f6-20ff-11df-95c0-0d452a75c466>
2010-07-14 02:58:50 +0000

    try avrdude

--- mcu/program.sh
+++ mcu/program.sh
@@ -4,14 +4,15 @@
 #
 #
 
-HEX=obj/main.hex:i  # :i for intel hex - only important for read
+HEX=obj/mcu.hex:i  # :i for intel hex - only important for read
 
 PORT=/dev/ttyUSB0
 MCU_TARGET=m328p    # Atmega328p
 PROGRAMMER=stk500v2 
+BAUD=115200
 
 FUSE=               # -u
 MEMTYPE=flash       # flash, eeprom, hfuse, lfuse, efuse
 OPERATION=w         # r|w|v
 
-avrdude -p $MCU_TARGET -c $PROGRAMMER -P $PORT -U $MEMTYPE:$OPERATION:$HEX
+avrdude -p $MCU_TARGET -c $PROGRAMMER -P $PORT -b $BAUD -U $MEMTYPE:$OPERATION:$HEX
