9ea249a2fa70232d756d763b6c254c8f834551f0
liamosullivan <liamosullivan@9b9702f6-20ff-11df-95c0-0d452a75c466>
2010-08-28 05:46:55 +0000

    Flight computer files changed to implement kf

--- airborne/flight_computer/src/main.c
+++ airborne/flight_computer/src/main.c
@@ -10,7 +10,7 @@
  * Queensland University of Technology
  *
  * @section DESCRIPTION
- * Flight computer main
+ * Flight computer main program
  *
  */
 
@@ -23,11 +23,14 @@
 #include "state.h"
 #include "imuserial.h"
 #include "arduserial.h"
+#include "kf.h"
 
 // udp server
 static Server server;
 // state variable
 state_t state;
+// sensor data variable
+sensor_data_t raw_IMU;
 
 pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
@@ -51,6 +54,8 @@ int main(int argc, char *argv[])
   sensorReady = sensorInit();
   // initialise the MCU
   mcuReady = mcuInit();
+  // initialise the Kalman filter
+  attitudeFilterInitialise();
   // all systems operational - begin the flight computer
   if (sensorReady && mcuReady)
   {
@@ -92,13 +97,30 @@ void * updateCompassHeading(void *pointer)
 // update IMU data
 void * updateIMUdata(void *pointer)
 {
+  // time stamp
+  struct timeval timestamp;
+  // time between filter updates
+  double startFilterTime, endFilterTime, diffFilterTime;
+  // calculate filter start time
+  gettimeofday(&timestamp, NULL); 
+  startFilterTime = timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
   while(1)
   {
     // sleep the thread for updating the compass
     usleep(IMU_DELAY*1e3);
     pthread_mutex_lock(&mut);
     // get the IMU sensor data
-    getImuSensorData(&state.p, &state.q, &state.r, &state.ax, &state.ay, &state.az);
+    //getImuSensorData(&state.p, &state.q, &state.r, &state.ax, &state.ay, &state.az);
+    getImuSensorData(&raw_IMU.p, &raw_IMU.q, &raw_IMU.r, &raw_IMU.ax, &raw_IMU.ay, &raw_IMU.az);
+    // calculate the time elapsed since last update
+    gettimeofday(&timestamp, NULL); 
+    endFilterTime = timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
+    diffFilterTime = endFilterTime - startFilterTime;
+    // calculate filter start time
+    gettimeofday(&timestamp, NULL); 
+    startFilterTime = timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
+    // perform the attitude filtering using the imu data
+    attitudeFilter(&raw_IMU.p, &raw_IMU.q, &raw_IMU.r, &raw_IMU.ax, &raw_IMU.ay, &raw_IMU.az, &state.phi, &state.theta, &state.psi, diffFilterTime);
     pthread_mutex_unlock(&mut);
   }
   return NULL;
@@ -119,7 +141,7 @@ int sensorInit()
     printf(">> IMU connected\n");
   } else
     {
-      printf(">> Error: Cannot connect to IMU\n");
+      printf(">> Error: Cannot connect to IMU on serial port: %s\n",IMU_SERIAL_PORT);
     }
   // connect to the arduino
   retSerial = openArduSerial(ARDU_SERIAL_PORT, ARDU_BAUD_RATE);
@@ -129,7 +151,7 @@ int sensorInit()
     printf(">> Arduino connected\n");
   } else
     {
-      printf(">> Error: Cannot connect to Arduino\n");
+      printf(">> Error: Cannot connect to Arduino on serial port: %s\n", ARDU_SERIAL_PORT);
     }
 
   return retValue;
@@ -138,7 +160,7 @@ int sensorInit()
 // initialise connection to the MCU
 int mcuInit()
 {
-  printf(">> MCU connected\n");
+  printf(">> MCU connected \n");
   return 1;
 }
 
@@ -150,12 +172,14 @@ void * sendUDPData(void *pointer)
   int init = 1;
   int count=0;
   struct timeval timestamp;
-  double startTime = 0;
-  double endTime = 0;
-  double diffTime = 0;
+  double startServerTime = 0;
+  double endServerTime = 0;
+  double diffServerTime = 0;
+  int dataLength;
+  unsigned char buffer[512];
   // initialise startTime
   gettimeofday(&timestamp, NULL); 
-  startTime=timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
+  startServerTime=timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
 
   while(1)
   {
@@ -180,16 +204,19 @@ void * sendUDPData(void *pointer)
     {
       // calculate server delay time
       gettimeofday(&timestamp, NULL); 
-      endTime=timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
-      diffTime = endTime - startTime;
+      endServerTime=timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
+      diffServerTime = endServerTime - startServerTime;
       // output the sent states  
-      printf(">> state : %f %f %f %f | %f\n",state.p,state.q,state.r,state.psi,(1/diffTime));
-      // send the server packet
+      printf(">> state : %f %f %f %f | %f\n",state.p,state.q,state.r,state.psi,(1/diffServerTime));
+      // send the state packet
       server_send_packet(&server, HELI_STATE, &state, sizeof(state_t));
+      // send the raw sensor data packet
+      dataLength = PackSensorData(buffer, &raw_IMU);
+      server_send_packet(&server, SENSOR_DATA, buffer, dataLength);
       init = 0;
       // reinitialise startTime
       gettimeofday(&timestamp, NULL); 
-      startTime=timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
+      startServerTime=timestamp.tv_sec+(timestamp.tv_usec/1000000.0);
     }   
   }
   return NULL;
