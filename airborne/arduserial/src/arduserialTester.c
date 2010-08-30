612161efda9040b42c090af933d323bc6c1bfd4d
liamosullivan <liamosullivan@9b9702f6-20ff-11df-95c0-0d452a75c466>
2010-08-30 10:17:14 +0000

    Changed arduserial

--- airborne/arduserial/src/arduserialTester.c
+++ airborne/arduserial/src/arduserialTester.c
@@ -20,6 +20,7 @@
 char* arduino_serial_port;
 double compassHeading = 0.0;
 double batteryVoltage = 0.0;
+double altitudeReading = 0.0;
 
 int main(int argc, char* argv[])
 {
@@ -59,7 +60,7 @@ int main(int argc, char* argv[])
     {
       printf("%.1lf\nTest 2: Success - Read compass heading\n",compassHeading);
     }
-  // Test 3: read compass heading
+  // Test 3: read battery voltage
   if (!getBatteryVoltage(&batteryVoltage))
   {
     fprintf(stderr,"Test 3: Error - Cannot get battery voltage\n");
@@ -68,12 +69,21 @@ int main(int argc, char* argv[])
     {
       printf("%.1lf\nTest 3: Success - Read battery voltage\n",batteryVoltage);
     }
-  // Test 4: read 10 compass heading
+  // Test 4: read altitude reading
+  if (!getAltitudeReading(&altitudeReading))
+  {
+    fprintf(stderr,"Test 4: Error - Cannot get altitude reading\n");
+    return -1;
+  } else
+    {
+      printf("%.1lf\nTest 4: Success - Read altitude sensor\n",batteryVoltage);
+    }
+  // Test 5: read 10 compass heading
   for(i=0; i<10; i++)
   {
     if (!getCompassHeading(&compassHeading))
     {
-      fprintf(stderr,"Test 4: Error - Cannot get compass heading\n");
+      fprintf(stderr,"Test 5: Error - Cannot get compass heading\n");
       return -1;
     } else
     {
@@ -82,7 +92,7 @@ int main(int argc, char* argv[])
   }
   if (i == 10)
   {
-    printf("Test 4: Success - Read 10 compass headings\n");
+    printf("Test 5: Success - Read 10 compass headings\n");
   }
   // Close the arduino serial port
   closeArduSerial();
